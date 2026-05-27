9f9d45e54f7e26897ee72a1cbe56314c256592e8
mathias <mathigmtk@gmail.com>
2026-05-27 20:50:25 -0300

    commit inicial

---
+++ openink/src/main/java/com/univille/openink/OpeninkApplication.java
@@ -0,0 +1,13 @@
+package com.univille.openink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpeninkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpeninkApplication.class, args);
+	}
+
+}
